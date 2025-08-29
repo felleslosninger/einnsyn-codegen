612eb8cf88b9cff7f9201f1a66cb0f5ef2b075f1
Gisle Nes <gisle@gisle.net>
2025-08-29 12:31:36 +0200

    Exclude redundant models

--- src/targets/sdk-java/emitUnknownModels.ts
+++ src/targets/sdk-java/emitUnknownModels.ts
@@ -7,18 +7,21 @@ import {
 	resolvePath,
 } from "@typespec/compiler";
 import {
-	buildGeneralModel,
-	getEnums,
-} from "../../languages/java/helpers/modelBuilder.js";
-import {
 	getJavaModelPackageName,
 	getJavaModelPathName,
 } from "../../languages/java/helpers/javaHelpers.js";
+import {
+	buildGeneralModel,
+	getEnums,
+} from "../../languages/java/helpers/modelBuilder.js";
 import { JavaFile } from "../../languages/java/primitives/javafile.js";
 import type { JavaBaseProps } from "../../languages/java/types.js";
-import { recursivelyGetModels } from "../../utils/getters.js";
-import { isEInnsynEntity } from "../../utils/typecheckers.js";
+import {
+	getBodyPropertyType,
+	recursivelyGetModels,
+} from "../../utils/getters.js";
 import { getFlattenedModel } from "../../utils/modelUtils.js";
+import { isEInnsynEntity } from "../../utils/typecheckers.js";
 
 export function emitUnknownModels(
 	context: EmitContext,
@@ -37,7 +40,15 @@ export function emitUnknownModels(
 		// No generic models:
 		.filter((model) => !isTemplateDeclaration(model))
 		// No error models
-		.filter((model) => !isErrorModel(context.program, model));
+		.filter((model) => !isErrorModel(context.program, model))
+		// Don't emit models that has another emitted model as @body
+		.filter((model) => {
+			const bodyProperty = getBodyPropertyType(model);
+			if (bodyProperty?.kind === "Model" && bodyProperty.name) {
+				return false;
+			}
+			return true;
+		});
 
 	for (const model of models) {
 		// Inheritance complicates things unnecessarily, so we flatten the model
