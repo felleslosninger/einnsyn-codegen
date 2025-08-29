41621677fc9b21f22a85dcb75b055efbbe6c6172
Gisle Nes <gisle@gisle.net>
2025-08-29 10:09:53 +0200

    Don't emit models with an already emitted model as `@body`

--- src/targets/java-backend/emitUnknownModels.ts
+++ src/targets/java-backend/emitUnknownModels.ts
@@ -6,14 +6,17 @@ import {
 	type Namespace,
 	resolvePath,
 } from "@typespec/compiler";
-import { buildGeneralModel } from "../../languages/java/helpers/modelBuilder.js";
 import {
 	getJavaModelPackageName,
 	getJavaModelPathName,
 } from "../../languages/java/helpers/javaHelpers.js";
+import { buildGeneralModel } from "../../languages/java/helpers/modelBuilder.js";
 import { JavaFile } from "../../languages/java/primitives/javafile.js";
 import type { JavaBaseProps } from "../../languages/java/types.js";
-import { recursivelyGetModels } from "../../utils/getters.js";
+import {
+	getBodyPropertyType,
+	recursivelyGetModels,
+} from "../../utils/getters.js";
 import { isEInnsynEntity } from "../../utils/typecheckers.js";
 
 export function emitUnknownModels(
@@ -33,7 +36,15 @@ export function emitUnknownModels(
 		// No generic models:
 		.filter((model) => !isTemplateDeclaration(model))
 		// No error models
-		.filter((model) => !isErrorModel(context.program, model));
+		.filter((model) => !isErrorModel(context.program, model))
+		// Don't emit models that has another emitted model as @body
+		.filter((model) => {
+			const bodyProperty = getBodyPropertyType(model);
+			if (bodyProperty?.kind === "Model" && bodyProperty.name) {
+				return false;
+			}
+			return true;
+		});
 
 	for (const model of models) {
 		// Create java file
